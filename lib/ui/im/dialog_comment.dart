3172f365b44767b8570b061e2b868da4676fec5c
吕川 <52621812@qq.com>
2022-09-23 00:53:20 +0800

    接口

---
+++ lib/ui/im/dialog_comment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class dialog_comment extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container()
+    );
+  }
+}
