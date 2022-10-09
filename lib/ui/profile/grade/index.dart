cbd3a56d08e7a0839666fc49506c3720661383c1
machun <machun@essex.com.cn>
2022-10-09 21:16:14 +0800

    大神等级页面 资源文件

--- lib/ui/profile/grade/index.dart
+++ lib/ui/profile/grade/index.dart
@@ -23,43 +23,116 @@ class GradePage extends GetView<GradeController> {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Stack(children: [Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Stack(
               children: [
-                Container(
-                  width: 200,
-                  height: 200,
-                  child: Stack(
-                    children: [
-                      ArcProgressBar(
-                        progress: 50,
-                        width: 200,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 230,
+                      width: Get.width,
+                      alignment: Alignment.center,
+                      child: Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              image: new DecorationImage(
+                                scale: 1.6,
+                                fit: BoxFit.scaleDown,
+                                image: AssetImage(
+                                    'assets/images/grade/baron.webp'),
+                              ),
+                            ),
+                            width: 212,
+                            child: ArcProgressBar(
+                              progress: 80,
+                            ),
+                          ),
+                          Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Spacer(),
+                                  Image(
+                                    image: AssetImage(
+                                        'assets/images/grade/v_grade1.webp'),
+                                    height: 32,
+                                  ),
+                                  Spacer(),
+                                  Image(
+                                    image: AssetImage(
+                                        'assets/images/grade/v_grade2.webp'),
+                                    height: 32,
+                                  ),
+                                  Spacer()
+                                ],
+                              )),
+                          Positioned(
+                              left: 10,
+                              right: 10,
+                              bottom: 25,
+                              child: Center(
+                                child: PWidget.text('LEVEL',
+                                    [Colors.white, 18, true], {'ff': 'DIN'}),
+                              )),
+                        ],
                       ),
-                      Positioned(
-                          left: 0,
-                          right: 0,
-                          top: 0,
-                          bottom: 0,
-                          child: Icon(Icons.access_alarm,size: 132,)),
-                      Positioned(
-                          bottom: 0,
-                          left: 0,
-                          right: 0,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Image(image: AssetImage('assets/images/grade/grade1.webp'),height: 32,),
-                              Image(image: AssetImage('assets/images/grade/v_grade1.webp'),height: 32,),
-                            ],
-                          ))
-                    ],
-                  ),
-                ),
+                    ),
+                  ],
+                )
               ],
-            )],),
+            ),
             PWidget.boxh(40),
-            PWidget.text('Current level',[Colors.white, 18, true], {'ff': 'DIN'})
+            PWidget.text(
+                'Current level', [Colors.white, 18, true], {'ff': 'DIN'}),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                buildScoreItem('recharge'),
+                buildScoreItem('consumption')
+              ],
+            ),
+            nextLevelButton(),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                buildScoreItem('recharge'),
+                buildScoreItem('consumption')
+              ],
+            )
           ],
         ),
       );
+
+  buildScoreItem(var title) {
+    return Expanded(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        PWidget.boxh(16),
+        PWidget.text(
+            'Monthly $title', [Color.fromRGBO(255, 255, 255, 0.65), 14, true]),
+        PWidget.boxh(8),
+        PWidget.text('£ 5000', [Colors.white, 30, true], {'ff': 'DIN'}),
+        PWidget.boxh(10),
+      ],
+    ));
+  }
+
+  nextLevelButton() {
+    return Column(
+      children: [
+        Image(
+          image: AssetImage('assets/images/grade/arrow_down.webp'),
+          height: 22,
+        ),
+        PWidget.boxh(22),
+        PWidget.text('Next Level', [Colors.white, 18, true], {'ff': 'DIN'})
+      ],
+    );
+  }
 }
