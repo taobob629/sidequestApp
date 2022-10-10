c009360fd9453710d9144da0a4b301e7679ba129
mc <>
2022-10-10 17:05:12 +0800

    大神接口

--- lib/ui/profile/grade/controller.dart
+++ lib/ui/profile/grade/controller.dart
@@ -6,25 +6,83 @@
   Copyright © sidequest_hub_app. All rights reserved.
 */
 import 'package:get/get.dart';
+import 'package:wy/api/user_api.dart';
+import 'package:wy/model/level_model.dart';
+import 'package:wy/model/user_info_model.dart';
+import 'package:wy/ui/controller/user_controller.dart';
 
-class GradeController extends GetxController{
- RxBool _isLoadding=RxBool(true);
+class GradeController extends GetxController {
+  RxBool _isLoadding = RxBool(true);
 
- bool get isLoadding => _isLoadding.value;
+  bool get isLoadding => _isLoadding.value;
 
   set isLoadding(bool value) {
     _isLoadding.value = value;
   }
 
+  UserController userController = Get.find<UserController>();
+  late LevelModel model;
+  late int isauth;
+
   @override
   void onInit() {
     super.onInit();
-    Future.delayed(Duration(milliseconds: 2000),(){
-      isLoadding=false;
-    });
+    isauth = userController.userInfoModel.value.isauth;
+    initData();
   }
-  //获取等级数据
-  initData(){
 
+  //获取等级数据
+  initData() async {
+    model = await UserApi.level();
+    isLoadding = false;
   }
+
+  String centerImg() {
+    var img;
+    int level = model.userLevel;
+    if (isauth != TYPE_VIP) {
+      switch (level) {
+        case 0:
+        case 1:
+          img = 'assets/images/grade/baron.webp';
+          break;
+        case 2:
+          img = 'assets/images/grade/viscounet.webp';
+          break;
+        case 3:
+          img = 'assets/images/grade/earl.webp';
+          break;
+        case 4:
+          img = 'assets/images/grade/marquis.webp';
+          break;
+        case 5:
+          img = 'assets/images/grade/duke.webp';
+          break;
+      }
+    } else {
+      if (level == 0) {
+        img = 'assets/images/grade/level1.webp';
+      } else {
+        img = 'assets/images/grade/level$level.webp';
+      }
+    }
+    return img;
+  }
+
+  curLevelImg() {
+    if (model.userLevel == 0) {
+      if (isauth == TYPE_VIP)
+        return 'assets/images/grade/${isauth == TYPE_VIP ? 'v_' : ''}grade1.webp';
+    }
+    return 'assets/images/grade/${isauth == TYPE_VIP ? 'v_' : ''}grade${model.userLevel}.webp';
+  }
+
+  nextLevelImg(){
+    if (model.userLevel == 0) {
+      if (isauth == TYPE_VIP)
+        return 'assets/images/grade/${isauth == TYPE_VIP ? 'v_' : ''}grade1.webp';
+    }
+    return 'assets/images/grade/${isauth == TYPE_VIP ? 'v_' : ''}grade${model.userLevel+1}.webp';
+  }
+
 }
--- lib/ui/profile/grade/index.dart
+++ lib/ui/profile/grade/index.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:wy/model/user_info_model.dart';
 import 'package:wy/ui/profile/grade/controller.dart';
 import 'package:wy/widget/arc_progressbar_widget.dart';
 import 'package:wy/widget/paixs_widget.dart';
@@ -21,95 +22,102 @@ class GradePage extends GetView<GradeController> {
           centerTitle: true,
           elevation: 0,
         ),
-        body:Obx(()=>controller.isLoadding?buildLoad(): Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Stack(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 230,
-                      width: Get.width,
-                      alignment: Alignment.center,
-                      child: Stack(
+        body: Obx(() => controller.isLoadding
+            ? buildLoad()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Container(
-                            decoration: BoxDecoration(
-                              image: new DecorationImage(
-                                scale: 1.6,
-                                fit: BoxFit.scaleDown,
-                                image: AssetImage(
-                                    'assets/images/grade/baron.webp'),
-                              ),
-                            ),
-                            width: 212,
-                            child: ArcProgressBar(
-                              progress: 80,
+                            height: 230,
+                            width: Get.width,
+                            alignment: Alignment.center,
+                            child: Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    image: new DecorationImage(
+                                      scale: 1.8,
+                                      fit: BoxFit.scaleDown,
+                                      image: AssetImage(controller.centerImg()),
+                                    ),
+                                  ),
+                                  width: 212,
+                                  child: ArcProgressBar(
+                                    progress: controller.model.percent,
+                                  ),
+                                ),
+                                Positioned(
+                                    bottom: 0,
+                                    left: 0,
+                                    right: 0,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Spacer(),
+                                        Image(
+                                          image: AssetImage(
+                                              controller.curLevelImg()),
+                                          height: 32,
+                                        ),
+                                        Spacer(),
+                                        Image(
+                                          image: AssetImage(
+                                              controller.nextLevelImg()),
+                                          height: 32,
+                                        ),
+                                        Spacer()
+                                      ],
+                                    )),
+                                Positioned(
+                                  left: 10,
+                                  right: 10,
+                                  bottom: 20,
+                                  child: Visibility(
+                                      visible: controller.isauth == TYPE_VIP,
+                                      child: Center(
+                                        child: PWidget.text(
+                                            'LEVEL${controller.model.userLevel}',
+                                            [Colors.white, 18, true],
+                                            {'ff': 'DIN'}),
+                                      )),
+                                ),
+                              ],
                             ),
                           ),
-                          Positioned(
-                              bottom: 0,
-                              left: 0,
-                              right: 0,
-                              child: Row(
-                                mainAxisAlignment:
-                                MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Spacer(),
-                                  Image(
-                                    image: AssetImage(
-                                        'assets/images/grade/v_grade1.webp'),
-                                    height: 32,
-                                  ),
-                                  Spacer(),
-                                  Image(
-                                    image: AssetImage(
-                                        'assets/images/grade/v_grade2.webp'),
-                                    height: 32,
-                                  ),
-                                  Spacer()
-                                ],
-                              )),
-                          Positioned(
-                              left: 10,
-                              right: 10,
-                              bottom: 25,
-                              child: Center(
-                                child: PWidget.text('LEVEL',
-                                    [Colors.white, 18, true], {'ff': 'DIN'}),
-                              )),
                         ],
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            PWidget.boxh(40),
-            PWidget.text(
-                'Current level', [Colors.white, 18, true], {'ff': 'DIN'}),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                buildScoreItem('recharge'),
-                buildScoreItem('consumption')
-              ],
-            ),
-            nextLevelButton(),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                buildScoreItem('recharge'),
-                buildScoreItem('consumption')
-              ],
-            )
-          ],
-        )),
+                      )
+                    ],
+                  ),
+                  PWidget.boxh(40),
+                  PWidget.text(
+                      'Current level', [Colors.white, 18, true], {'ff': 'DIN'}),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      buildScoreItem('recharge', controller.model.levelNum),
+                      buildScoreItem('consumption', controller.model.levelNum)
+                    ],
+                  ),
+                  nextLevelButton(),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      buildScoreItem('recharge', controller.model.nextLevelNum),
+                      buildScoreItem(
+                          'consumption', controller.model.nextLevelNum)
+                    ],
+                  )
+                ],
+              )),
       );
 
-  buildScoreItem(var title) {
+  buildScoreItem(var title, var value) {
     return Expanded(
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -118,7 +126,7 @@ class GradePage extends GetView<GradeController> {
         PWidget.text(
             'Monthly $title', [Color.fromRGBO(255, 255, 255, 0.65), 14, true]),
         PWidget.boxh(8),
-        PWidget.text('£ 5000', [Colors.white, 30, true], {'ff': 'DIN'}),
+        PWidget.text('£ $value', [Colors.white, 30, true], {'ff': 'DIN'}),
         PWidget.boxh(10),
       ],
     ));
