2af1e39324c0ba70f89a34235f85f360fd9409c9
paixs <2930118234@qq.com>
2022-12-02 20:58:30 +0800

    弹窗滚动问题

--- lib/ui/common/dialog_confirm.dart
+++ lib/ui/common/dialog_confirm.dart
@@ -12,19 +12,22 @@ class ConfirmDialog extends StatelessWidget {
   final String? concelBtn;
   final Function? onConfirm;
 
-  ConfirmDialog(
-      {required this.title,
-      required this.info,
-      this.cancelable = true,
-      this.confirmBtn = "CONFIRM",
-      this.concelBtn,
-      this.onConfirm});
+  ConfirmDialog({required this.title, required this.info, this.cancelable = true, this.confirmBtn = "CONFIRM", this.concelBtn, this.onConfirm});
 
   @override
   Widget build(BuildContext context) {
+    if (title.contains('Cancel Subscription') || title.contains('取消订阅')) {
+      return view2(context);
+    } else {
+      return view1(context);
+    }
+  }
+
+  ///可滚动
+  Widget view2(BuildContext context) {
     return WyDialog(
       child: Container(
-          height: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
@@ -37,8 +40,7 @@ class ConfirmDialog extends StatelessWidget {
                 physics: BouncingScrollPhysics(),
                 child: Container(
                   margin: EdgeInsets.symmetric(vertical: 30),
-                  child: Text("$info",
-                      style: TextStyle(color: Colors.white, fontSize: 14)),
+                  child: Text("$info", style: TextStyle(color: Colors.white, fontSize: 14)),
                 ),
               ),
             ),
@@ -51,10 +53,7 @@ class ConfirmDialog extends StatelessWidget {
                             padding: const EdgeInsets.only(top: 4),
                             child: Text(
                               "$concelBtn",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontFamily: "DIN"),
+                              style: TextStyle(color: Colors.white, fontSize: 18, fontFamily: "DIN"),
                             ),
                           ),
                           height: 40,
@@ -69,14 +68,11 @@ class ConfirmDialog extends StatelessWidget {
                     padding: const EdgeInsets.only(top: 4),
                     child: Text(
                       "$confirmBtn".tr,
-                      style: TextStyle(
-                          color: Colors.white, fontSize: 18, fontFamily: "DIN"),
+                      style: TextStyle(color: Colors.white, fontSize: 18, fontFamily: "DIN"),
                     ),
                   ),
                   height: 40,
-                  onTap: () => onConfirm == null
-                      ? Navigator.pop(context, true)
-                      : onConfirm!.call(),
+                  onTap: () => onConfirm == null ? Navigator.pop(context, true) : onConfirm!.call(),
                 ))
               ],
             )
@@ -86,15 +82,64 @@ class ConfirmDialog extends StatelessWidget {
     );
   }
 
-  static Future<bool?> show(BuildContext context, String title, String info,
-      {bool cancelable = true}) async {
+  ///不可滚动
+  Widget view1(BuildContext context) {
+    return WyDialog(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontSize: 16, color: Colors.white),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 30),
+            child: Text("$info", style: TextStyle(color: Colors.white, fontSize: 14)),
+          ),
+          Row(
+            children: [
+              if (concelBtn != null)
+                Flexible(
+                    child: ColorfulButton(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 4),
+                          child: Text(
+                            "$concelBtn",
+                            style: TextStyle(color: Colors.white, fontSize: 18, fontFamily: "DIN"),
+                          ),
+                        ),
+                        height: 40,
+                        onTap: () => Navigator.pop(context, true))),
+              if (concelBtn != null)
+                Container(
+                  width: 16,
+                ),
+              Flexible(
+                  child: ColorfulButton(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Text(
+                    "$confirmBtn".tr,
+                    style: TextStyle(color: Colors.white, fontSize: 18, fontFamily: "DIN"),
+                  ),
+                ),
+                height: 40,
+                onTap: () => onConfirm == null ? Navigator.pop(context, true) : onConfirm!.call(),
+              ))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  static Future<bool?> show(BuildContext context, String title, String info, {bool cancelable = true}) async {
     return await showDialog<bool>(
         context: context,
         barrierColor: Colors.black26,
         barrierDismissible: cancelable,
         builder: (BuildContext context) {
-          return ConfirmDialog(
-              title: title, info: info, cancelable: cancelable);
+          return ConfirmDialog(title: title, info: info, cancelable: cancelable);
         });
   }
 }
