7bc39271e1b2ce79c1ec7daaba06a3650530627f
taob <1277389320@qq.com>
2023-01-10 14:43:36 +0800

    去掉提现手续费说明

--- lib/ui/playwith/balance/my_earnings_page.dart
+++ lib/ui/playwith/balance/my_earnings_page.dart
@@ -112,7 +112,7 @@ class _MyEarningsPageState extends State<MyEarningsPage> {
         PWidget.column([
           PWidget.text('Withdrawal and exchange instructions:'.tr, [Color(0xffEEF3FF)]),
           Text(
-            '''1. ${'If you withdraw cash from us, you’ll be charged a handling fee of 8%.'.tr}\n2. ${'Withdrawals typically take three to five bank working days.'.tr}\n3. ${'6 Diamond for £1.'.tr}''',
+            '''1. ${'Withdrawals typically take three to five bank working days.'.tr}\n2. ${'6 Diamond for £1.'.tr}''',
             style: TextStyle(color: Color(0xff8291B4)),
           ),
         ]),
