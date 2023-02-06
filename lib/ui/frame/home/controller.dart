6e002f0bb027cf1464653cee93fb022e04970ef0
mc <>
2023-02-06 16:13:48 +0800

    新增首页目录

---
+++ lib/ui/frame/home/controller.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:wy/api/user_api.dart';
+import 'package:wy/common/getx_list_controller.dart';
+import 'package:wy/model/simple_user_info_model.dart';
+
+/**
+    author:mac
+    创建日期:2023/2/2
+    描述:
+ */
+class HomePageController extends GetxListController<SimpleUserInfoModel> {
+  late FocusNode focusNode;
+  late TextEditingController controller;
+
+  @override
+  void onInit() {
+    super.onInit();
+    focusNode = FocusNode();
+    controller = TextEditingController();
+  }
+
+  @override
+  void onClose() {
+    controller.dispose();
+    focusNode.dispose();
+    super.onClose();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    focusNode.requestFocus();
+  }
+
+  @override
+  Future<List<SimpleUserInfoModel>> loadData() async {
+    String key = controller.text;
+    if (key.isEmpty) {
+      return [];
+    }
+    EasyLoading.show();
+    List<SimpleUserInfoModel> list = await UserApi.search(key);
+    EasyLoading.dismiss();
+    return list;
+  }
+}
---
+++ lib/ui/frame/messages/view.dart
@@ -0,0 +1,7 @@
+/*
+  view
+  sidequest_hub_app
+  desc:
+  Created by chunma on .
+  Copyright © sidequest_hub_app. All rights reserved.
+*/
