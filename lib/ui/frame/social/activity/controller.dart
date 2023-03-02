fe67cbcf63f035822a8d9fadf73a39f944f90699
machun <machun@essex.com.cn>
2023-03-02 22:53:05 +0800

    activity默认选中tab

--- lib/ui/frame/social/activity/controller.dart
+++ lib/ui/frame/social/activity/controller.dart
@@ -7,7 +7,8 @@ import 'package:get/get.dart';
 import 'package:wy/api/activity_api.dart';
 import 'package:wy/common/base_tab_controller.dart';
 import 'package:wy/model/activity_tab.dart';
-class ActivityTabController<ActivityTabModel> extends BaseTabContoller {
+
+class ActivityTabController extends BaseTabContoller<ActivityTabModel> {
   Rxn<ActivityTabModel> _curTab = Rxn();
 
   ActivityTabModel? get curTab => _curTab.value;
@@ -17,9 +18,12 @@ class ActivityTabController<ActivityTabModel> extends BaseTabContoller {
   }
 
   @override
- Future initTabs() async {
-   List tabs = await ActivityApi.activityTabs();
-    if (tabs.isNotEmpty) curTab = tabs[0];
+  Future initTabs() async {
+    List<ActivityTabModel> tabs = await ActivityApi.activityTabs();
+    var defaultTab = tabs.firstWhereOrNull((tab) => tab.defaut == 1);
+    //默认展示
+    // if (tabs.isNotEmpty) curTab = tabs[0];
+    curTab = defaultTab ?? tabs[0];
     return tabs;
   }
 }
