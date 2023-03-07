01c0fca77a6a8c40a19e6602f20828629ee5531f
machun <machun@essex.com.cn>
2023-03-07 16:55:01 +0800

    是否在线的判断

--- lib/ui/frame/main_page.dart
+++ lib/ui/frame/main_page.dart
@@ -340,22 +340,22 @@ class MainPageController extends FullLifeCycleController with FullLifeCycleMixin
     if (checking == false) {
       checking = true;
       IndexApi.checkVersion().then((value) {
-        // final profilePageController = ProfilePageController.instance();
-        // if (Platform.isIOS) {
-        //   StorageManager.setOnline(value.status);
-        // } else {
-        //   StorageManager.setOnline(true);
-        // }
-        // profilePageController.online.value = StorageManager.getOnline();
-        // if (value.upgrade) {
-        //   if (Get.context != null) {
-        //     UpgradeDialog.show(Get.context!, value, cancelable: !value.force).whenComplete(() => checkAd(Get.context!));
-        //   }
-        // } else {
-        //   if (Get.context != null) {
-        //     checkAd(Get.context!);
-        //   }
-        // }
+      //  final profilePageController = ProfilePageController.instance();
+        if (Platform.isIOS) {
+          StorageManager.setOnline(value.status);
+        } else {
+          StorageManager.setOnline(true);
+        }
+      //  profilePageController.online.value = StorageManager.getOnline();
+        if (value.upgrade) {
+          if (Get.context != null) {
+            UpgradeDialog.show(Get.context!, value, cancelable: !value.force).whenComplete(() => checkAd(Get.context!));
+          }
+        } else {
+          if (Get.context != null) {
+            checkAd(Get.context!);
+          }
+        }
       });
     }
   }
