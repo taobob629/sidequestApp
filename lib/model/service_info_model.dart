0ecb880e17099fed6cee2f08dcfa061d9b9f1765
machun <machun@essex.com.cn>
2023-03-11 21:49:42 +0800

    技能编辑页面

---
+++ lib/model/service_info_model.dart
@@ -0,0 +1,81 @@
+
+import 'package:wy/model/safe_convert.dart';
+class ServiceInfoModel {
+  final List<SkillItem> skill;
+  // Mobile
+  final String name;
+  // 1
+  final String id;
+
+  ServiceInfoModel({
+    required this.skill,
+    this.name = "",
+    this.id = "",
+  });
+
+  factory ServiceInfoModel.fromJson(Map<String, dynamic>? json) => ServiceInfoModel(
+    skill: asT<List>(json, 'skill').map((e) => SkillItem.fromJson(e)).toList(),
+    name: asT<String>(json, 'name'),
+    id: asT<String>(json, 'id'),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'skill': skill.map((e) => e.toJson()).toList(),
+    'name': name,
+    'id': id,
+  };
+}
+
+class SkillItem {
+  final List<LevelItem> level;
+  // Clash Royale
+  final String name;
+  // 6
+  final String id;
+
+  SkillItem({
+    required this.level,
+    this.name = "",
+    this.id = "",
+  });
+
+  factory SkillItem.fromJson(Map<String, dynamic>? json) => SkillItem(
+    level: asT<List>(json, 'level').map((e) => LevelItem.fromJson(e)).toList(),
+    name: asT<String>(json, 'name'),
+    id: asT<String>(json, 'id'),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'level': level.map((e) => e.toJson()).toList(),
+    'name': name,
+    'id': id,
+  };
+}
+
+class LevelItem {
+  // Legendary Arena
+  final String name;
+  // 36
+  final int id;
+
+  LevelItem({
+    this.name = "",
+    this.id = 0,
+  });
+
+  factory LevelItem.fromJson(Map<String, dynamic>? json) => LevelItem(
+    name: asT<String>(json, 'name'),
+    id: asT<int>(json, 'id'),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'id': id,
+  };
+
+  @override
+  String toString() {
+    return 'LevelItem{name: $name, id: $id}';
+  }
+}
+
--- lib/ui/playwith/service/add/controller.dart
+++ lib/ui/playwith/service/add/controller.dart
@@ -8,11 +8,21 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:wy/api/game_api.dart';
 import 'package:wy/model/price_range_model.dart';
+import 'package:wy/model/service_info_model.dart';
 import 'package:wy/utils/utils.dart';
 
 class AddGamePageController extends GetxController {
   RxList<PriceRangeModel> priceRanges = RxList([]);
   RxList<PriceRangeModel> mPriceRanges = RxList([]); //我选择的技能列表
+  RxList<ServiceInfoModel> services = RxList([]);
+  ///是否编辑
+  bool isEdit = false;
+
+  initData() async {
+   var result=await GamesApi.getGameServicesInfo(isEdit);
+   services.addAll(result);
+
+  }
   getPriceRange(var gameId) async {
     var result = await GamesApi.getPriceRange(7);
     flog('priceResult $result');
@@ -22,19 +32,29 @@ class AddGamePageController extends GetxController {
   }
 
   onPriceUnitChange(int index, PriceRangeModel model) {
+    if (mPriceRanges[index] == model) return;
     if (mPriceRanges.contains(model)) {
       EasyLoading.showToast('已经存在改类型');
       return;
     }
-    mPriceRanges[index - 1] = model;
+    mPriceRanges[index] = model;
+  }
+
+  removePriceRange(int index) {
+    mPriceRanges.removeAt(index);
   }
 
   addPriceRange() {
-    if (mPriceRanges.isEmpty || mPriceRanges.length >= priceRanges.length) {
+    if (mPriceRanges.isEmpty) {
       mPriceRanges.add(priceRanges.first);
       EasyLoading.showToast('${'At most '.tr}${priceRanges.length}${' types can be added!'.tr} ');
       return;
     }
+    if (mPriceRanges.length >= priceRanges.length) {
+      EasyLoading.showToast('${'At most '.tr}${priceRanges.length}${' types can be added!'.tr} ');
+      return;
+    }
+
     //查看还有什么类型的没有被添加
     var item = priceRanges.firstWhereOrNull((element) => !mPriceRanges.contains(element));
     if (item != null) {
