d493aa7863d95f3a91e89d9794fe2ad270f8908a
zengchao928 <595018434@qq.com>
2023-04-03 11:26:10 +0800

    优化匹配弹框的重复弹窗

--- lib/ui/common/dialog_match_top.dart
+++ lib/ui/common/dialog_match_top.dart
@@ -14,242 +14,227 @@ import '../../model/match/match_order_player.dart';
 import '../../utils/count_down_util.dart';
 
 class MatchTopDialog extends StatelessWidget {
-  var countTime = '10s'.obs;
-  late CountDownUtil countDownUtil;
-  late MatchOrderPlayer player;
-
-  MatchTopDialog({required this.player}) {
-    countDownUtil = CountDownUtil(
-      (data) {
-        countTime.value = data;
-      },
-      () {
-        countDownUtil.stopCountDown();
-        Get.back();
-      },
-      seconds: 10,
-    );
-    countDownUtil.startCountDown();
-  }
+  final _ctr = Get.put(DialogMatchTopController());
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 150.h,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                ImageUtils.matchTopBg,
+    _ctr.initData();
+
+    return GetBuilder<DialogMatchTopController>(builder: (builder) {
+      return Column(
+        children: [
+          Container(
+            height: 150.h,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  ImageUtils.matchTopBg,
+                ),
+                fit: BoxFit.fill,
               ),
-              fit: BoxFit.fill,
             ),
-          ),
-          padding: EdgeInsets.only(left: 20.w, right: 6.w),
-          margin: EdgeInsets.symmetric(horizontal: 10.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 30.h,
-                margin: EdgeInsets.only(left: 14.w, bottom: 10.h),
-                child: Row(
-                  children: [
-                    Text(
-                      'I want to invite you to play the games'.tr,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14.sp,
-                        fontFamily: FONT_MEDIUM,
+            padding: EdgeInsets.only(left: 20.w, right: 6.w),
+            margin: EdgeInsets.symmetric(horizontal: 10.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 30.h,
+                  margin: EdgeInsets.only(left: 14.w, bottom: 10.h),
+                  child: Row(
+                    children: [
+                      Text(
+                        'I want to invite you to play the games'.tr,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.sp,
+                          fontFamily: FONT_MEDIUM,
+                        ),
                       ),
-                    ),
-                    Expanded(child: SizedBox()),
-                    Obx(
-                      () => GestureDetector(
-                        behavior: HitTestBehavior.translucent,
-                        onTap: () {
-                          countDownUtil.stopCountDown();
-                          Get.back();
-                        },
-                        child: Text(
-                          countTime.value,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 10.sp,
-                            fontFamily: FONT_MEDIUM,
+                      Expanded(child: SizedBox()),
+                      Obx(
+                            () => GestureDetector(
+                          behavior: HitTestBehavior.translucent,
+                          onTap: () {
+                            _ctr.countDownUtil.stopCountDown();
+                          },
+                          child: Text(
+                            _ctr.countTime.value,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10.sp,
+                              fontFamily: FONT_MEDIUM,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    GestureDetector(
-                      behavior: HitTestBehavior.translucent,
-                      onTap: () {
-                        countDownUtil.stopCountDown();
-                        Get.back();
-                      },
-                      child: Icon(
-                        Icons.close,
-                        color: Colors.white,
-                        size: 15.sp,
+                      GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () {
+                          _ctr.countDownUtil.stopCountDown();
+                        },
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.white,
+                          size: 15.sp,
+                        ),
+                      ),
+                      6.horizontalSpace,
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 50.w,
+                      height: 50.w,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          _ctr.player.avatar,
+                        ),
                       ),
                     ),
                     6.horizontalSpace,
-                  ],
-                ),
-              ),
-              Row(
-                children: [
-                  Container(
-                    width: 50.w,
-                    height: 50.w,
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        player.avatar,
-                      ),
-                    ),
-                  ),
-                  6.horizontalSpace,
-                  Expanded(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              player.nickname,
-                              style: TextStyle(
-                                color: Color(0xff333333),
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14.sp,
-                                fontFamily: FONT_MEDIUM,
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                _ctr.player.nickname,
+                                style: TextStyle(
+                                  color: Color(0xff333333),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14.sp,
+                                  fontFamily: FONT_MEDIUM,
+                                ),
                               ),
-                            ),
-                            6.horizontalSpace,
-                            SexAndAgeWidget(
-                              sex: player.sex,
-                              age: player.age,
-                            ),
-                          ],
-                        ),
-                        6.verticalSpace,
-                        Row(
-                          children: [
-                            Text(
-                              '${player.game}',
-                              style: TextStyle(
-                                color: Color(0xff666666),
-                                fontWeight: FontWeight.bold,
-                                fontSize: 11.sp,
-                                fontFamily: FONT_MEDIUM,
+                              6.horizontalSpace,
+                              SexAndAgeWidget(
+                                sex: _ctr.player.sex,
+                                age: _ctr.player.age,
                               ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Spacer(),
-                            Text(
-                              '${player.unit} x${player.quantity}',
-                              style: TextStyle(
-                                color: Color(0xff666666),
-                                fontWeight: FontWeight.bold,
-                                fontSize: 11.sp,
-                                fontFamily: FONT_MEDIUM,
-                              ),
-                            ),
-                            6.horizontalSpace,
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 56.w),
-                child: Text(
-                  player.requests,
-                  style: TextStyle(
-                    color: Color(0xff666666),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 11.sp,
-                    fontFamily: FONT_MEDIUM,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  children: [
-                    Spacer(),
-                    GestureDetector(
-                      behavior: HitTestBehavior.translucent,
-                      onTap: () => joinGame(1),
-                      child: Container(
-                        width: 70.w,
-                        height: 32.h,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.r),
-                            color: Color(0x70ffffff)),
-                        margin: EdgeInsets.only(right: 10.w),
-                        child: Center(
-                          child: Text(
-                            "Reject".tr,
-                            style: TextStyle(
-                              color: Color(0xff666666),
-                              fontFamily: FONT_MEDIUM,
-                              fontSize: 13.sp,
-                            ),
+                            ],
                           ),
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      behavior: HitTestBehavior.translucent,
-                      onTap: () => joinGame(0),
-                      child: Container(
-                        width: 70.w,
-                        height: 32.h,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20.r),
-                            gradient: LinearGradient(
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight,
-                                colors: [
-                                  Color(0xFFD49C21),
-                                  Color(0xFFE96524)
-                                ])),
-                        child: Center(
-                          child: Text(
-                            "Join".tr,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: FONT_MEDIUM,
-                                fontSize: 13.sp),
+                          6.verticalSpace,
+                          Row(
+                            children: [
+                              Text(
+                                '${_ctr.player.game}',
+                                style: TextStyle(
+                                  color: Color(0xff666666),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 11.sp,
+                                  fontFamily: FONT_MEDIUM,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Spacer(),
+                              Text(
+                                '${_ctr.player.unit} x${_ctr.player.quantity}',
+                                style: TextStyle(
+                                  color: Color(0xff666666),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 11.sp,
+                                  fontFamily: FONT_MEDIUM,
+                                ),
+                              ),
+                              6.horizontalSpace,
+                            ],
                           ),
-                        ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-              )
-            ],
+                Padding(
+                  padding: EdgeInsets.only(left: 56.w),
+                  child: Text(
+                    _ctr.player.requests,
+                    style: TextStyle(
+                      color: Color(0xff666666),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 11.sp,
+                      fontFamily: FONT_MEDIUM,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Spacer(),
+                      GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () => joinGame(1),
+                        child: Container(
+                          width: 70.w,
+                          height: 32.h,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.r),
+                              color: Color(0x70ffffff)),
+                          margin: EdgeInsets.only(right: 10.w),
+                          child: Center(
+                            child: Text(
+                              "Reject".tr,
+                              style: TextStyle(
+                                color: Color(0xff666666),
+                                fontFamily: FONT_MEDIUM,
+                                fontSize: 13.sp,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        behavior: HitTestBehavior.translucent,
+                        onTap: () => joinGame(0),
+                        child: Container(
+                          width: 70.w,
+                          height: 32.h,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20.r),
+                              gradient: LinearGradient(
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                  colors: [
+                                    Color(0xFFD49C21),
+                                    Color(0xFFE96524)
+                                  ])),
+                          child: Center(
+                            child: Text(
+                              "Join".tr,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: FONT_MEDIUM,
+                                  fontSize: 13.sp),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    });
   }
 
   void joinGame(int operation) async {
-    countDownUtil.stopCountDown();
-    Get.back();
+    _ctr.countDownUtil.stopCountDown();
 
     EasyLoading.show();
     Map<String, dynamic> params = {"operation": operation};
     List<MatchOperationModel>? result =
-        await MatchApi.acceptMatchOrder(player.orderId.toString(), params);
+    await MatchApi.acceptMatchOrder(_ctr.player.orderId.toString(), params);
     EasyLoading.dismiss();
 
     if (result == null) {
@@ -273,7 +258,8 @@ class MatchTopDialog extends StatelessWidget {
         tags: element.orderInfo.types,
         category: element.orderInfo.category,
         game: element.orderInfo.game,
-        priceRange: '${element.orderInfo.minPrice}~${element.orderInfo.maxPrice}',
+        priceRange:
+        '${element.orderInfo.minPrice}~${element.orderInfo.maxPrice}',
         unit: element.orderInfo.unit,
         launguage: element.orderInfo.language,
         skillAuthId: element.skillAuthId,
@@ -287,5 +273,27 @@ class MatchTopDialog extends StatelessWidget {
       AppPages.side_kick_match_suc_page,
       arguments: beans,
     );
+  }
+}
+
+class DialogMatchTopController extends GetxController {
+  var countTime = '10s'.obs;
+  late CountDownUtil countDownUtil;
+  late MatchOrderPlayer player;
+
+  void initData() {
+    final map = Get.arguments as Map;
+    player = map['player'];
+
+    countDownUtil = CountDownUtil(
+          (data) {
+        countTime.value = data;
+      },
+          () {
+        countDownUtil.stopCountDown();
+      },
+      seconds: map['seconds'],
+    );
+    countDownUtil.startCountDown();
   }
 }
