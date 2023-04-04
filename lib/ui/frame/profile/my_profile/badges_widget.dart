3643c95ac5527321fb83770695f317555767d49f
davi <804033869@qq.com>
2023-04-04 16:12:41 +0800

    增大字号

--- lib/ui/frame/profile/my_profile/badges_widget.dart
+++ lib/ui/frame/profile/my_profile/badges_widget.dart
@@ -10,6 +10,7 @@ import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:wy/config/app_color.dart';
 import 'package:wy/config/icon_font.dart';
+import 'package:wy/res/index.dart';
 import 'package:wy/ui/frame/profile/model/profile_model.dart';
 import 'package:wy/utils/index.dart';
 
@@ -21,46 +22,57 @@ class BadgesWidget extends GetView<ProfileController> {
   BadgesWidget(this.badge);
 
   var itemSize;
+  var cardItemSize;
 
   @override
   Widget build(BuildContext context) {
-    itemSize = (Get.width - 30 * 2) / 6;
+    itemSize = (Get.width - 30 * 2 - 5 * 2) / 6;
+    cardItemSize = 120.h;
     return Container(
-      margin: EdgeInsets.only(top: 15.h),
-      constraints: BoxConstraints(minHeight: 120.h),
+      margin: EdgeInsets.only(top: 10.h),
+      padding: EdgeInsets.only(top: 5.h),
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.only(left: 38),
             child: Text(
               '${badge?.name}',
-              style: TextStyle(color: Colors.white, fontSize: 14.sp, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.bold),
             ),
           ),
           Container(
-            height: 100.h,
             margin: EdgeInsets.only(top: 10, left: 30, right: 30, bottom: 10),
-            //   padding: EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              color: Color(0xff313033),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Swiper(
-              outer: true,
-              loop: false,
-              itemBuilder: (BuildContext context, int index) {
-                return Row(
-                  children: badge.getPageData(index).map((e) => badgeItem(e)).toList(),
-                );
-              },
-              itemCount: badge.getPageSize(),
-              pagination: SwiperPagination(
-                  builder: RectSwiperPaginationBuilder(
-                      color: AppColor.greyAF,
-                      activeColor: AppColor.yellow,
-                      size: Size(10, 10),
-                      activeSize: Size(18, 10))),
+            decoration: itemDecoration(),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: 110.h, maxHeight: 110.h),
+              child: Swiper(
+                outer: false,
+                loop: false,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: EdgeInsets.all(5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: badge
+                          .getPageData(index)
+                          .map((e) => badgeItem(e))
+                          .toList(),
+                    ),
+                  );
+                },
+                itemCount: badge.getPageSize(),
+                pagination: SwiperPagination(
+                    builder: RectSwiperPaginationBuilder(
+                        color: AppColor.greyAF,
+                        activeColor: AppColor.yellow,
+                        size: Size(10, 10),
+                        activeSize: Size(18, 10))),
+              ),
             ),
           )
         ],
@@ -69,33 +81,46 @@ class BadgesWidget extends GetView<ProfileController> {
   }
 
   Widget badgeItem(BadgeItem item) {
+    var iconSize = itemSize - 15;
     return Container(
       width: itemSize,
-      padding: EdgeInsets.all(5),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
         children: [
           if (item.lighted)
-            ImageUtil.networkImage(
-              url: item.iconImage,
-              width: itemSize - 10,
-              height: itemSize - 10,
-              fit: BoxFit.cover,
-            ),
-          if (item.lighted == false)
-            ColorFiltered(
-              colorFilter: ColorFilter.mode(Colors.grey.withOpacity(0.5), BlendMode.dstIn),
+            Container(
+              padding: EdgeInsets.all(5),
               child: ImageUtil.networkImage(
                 url: item.iconImage,
-                width: itemSize - 10,
-                height: itemSize - 10,
+                width: iconSize,
+                height: iconSize,
+                fit: BoxFit.cover,
               ),
             ),
+          if (item.lighted == false)
+            Container(
+              padding: EdgeInsets.all(5),
+              child: ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                    Colors.grey.withOpacity(0.5), BlendMode.dstIn),
+                child: ImageUtil.networkImage(
+                  url: item.iconImage,
+                  fit: BoxFit.cover,
+                  width: iconSize,
+                  height: iconSize,
+                ),
+              ),
+            ),
+          5.verticalSpace,
           Text(
             '${item.iconName}',
             textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white, fontSize: 6.sp, fontFamily: FONT_LIGHT),
+            maxLines: 2,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 10.sp,
+                overflow: TextOverflow.ellipsis,
+                fontFamily: FONT_LIGHT),
           )
         ],
       ),
