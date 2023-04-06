849c5d4f08e280b3c46459679d8edef0de307686
taob <1277389320@qq.com>
2023-04-06 18:24:59 +0800

    phone冲突

--- lib/ui/frame/profile/my_profile/profile_edit_page.dart
+++ lib/ui/frame/profile/my_profile/profile_edit_page.dart
@@ -468,7 +468,7 @@ class ProfileEditController extends GetxController {
   }
 
   updateProfile() {
-    ProfileApi.updateProfile(nickController.text,signatureController.text, phoneController.text, curLanguage.value, jsonEncode({"country": curCountry.value}), gender.value.toString()).then((value) {
+    ProfileApi.updateProfile(nickController.text,signatureController.text, phone.value, curLanguage.value, jsonEncode({"country": curCountry.value}), gender.value.toString()).then((value) {
       profileInit();
       Get.back();
     });
