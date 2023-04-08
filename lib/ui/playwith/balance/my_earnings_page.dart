682f6d4c34203ebe41a0da182e60519b32db7f9d
taob <1277389320@qq.com>
2023-04-08 19:23:47 +0800

    修改提现提示

--- lib/ui/playwith/balance/my_earnings_page.dart
+++ lib/ui/playwith/balance/my_earnings_page.dart
@@ -545,7 +545,7 @@ class WithdrawTipsDialog extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10)),
             child: PWidget.container(
               PWidget.column([
-                PWidget.text('Withdrawal and exchange instructions:'.tr,
+                PWidget.text('Withdrawal instructions:'.tr,
                     [Color(0xffEEF3FF)]),
                 Text(
                   '''1. ${'Withdrawals typically take three to five bank working days.'.tr}\n2. ${'6 Diamond for £1.'.tr}\n3. ${'If you withdraw cash from us, you’ll be charged a handling fee of 3%.'.tr}''',
