5ba293f7f16a5a0244593463ac0dcbd19ffda402
ShellPeng <swiftlyfish@gmail.com>
2023-04-13 21:02:35 +0800

    post tr

--- lib/ui/frame/social/controller.dart
+++ lib/ui/frame/social/controller.dart
@@ -10,6 +10,6 @@ import 'package:wy/common/base_tab_controller.dart';
 class SocialTabController extends BaseTabContoller {
   @override
   initTabs() {
-    tabs = ['Post', 'Activity'];
+    tabs = ['Post'.tr, 'Activity'.tr];
   }
 }
