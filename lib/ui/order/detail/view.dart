b61ae6f0c75261cae4a57d9c4c01a26e8f3e69b8
ShellPeng <swiftlyfish@gmail.com>
2023-04-13 11:36:13 +0800

    屏蔽聊天

--- lib/ui/order/detail/view.dart
+++ lib/ui/order/detail/view.dart
@@ -97,7 +97,7 @@ class OrderDetailPage extends GetView<OrderDetailPageController> {
             ),
             InkWell(
               child: ImageUtil.assetImage('ic_message_yellow', width: 36.w, height: 36.w),
-              onTap: () => controller.toChat(context),
+              // onTap: () => controller.toChat(context),
             )
           ],
         ),
