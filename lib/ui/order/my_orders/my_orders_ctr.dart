3eed0e857c461d2f408ac196f46576d14bd3d4c4
zengchao928 <595018434@qq.com>
2023-04-17 20:13:41 +0800

    订单添加loading

--- lib/ui/order/my_orders/my_orders_ctr.dart
+++ lib/ui/order/my_orders/my_orders_ctr.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 import 'package:wy/api/wy_http.dart';
 import 'package:wy/ui/controller/user_controller.dart';
@@ -45,6 +47,7 @@ class MyOrdersCtr extends GetxRefreshController<ServiceListModel> {
     if (!ifScaleBigReceived.value) {
       type = 1;
     }
+    EasyLoading.show();
 
     String url = '/peiwan/app/new/orders/list?type=$type&status=$selectStatus';
     var response = await http.get(url,
@@ -55,6 +58,7 @@ class MyOrdersCtr extends GetxRefreshController<ServiceListModel> {
     list = response.data['rows']
         .map<ServiceListModel>((item) => ServiceListModel.fromJson(item))
         .toList();
+    EasyLoading.dismiss();
     return list;
   }
 }
