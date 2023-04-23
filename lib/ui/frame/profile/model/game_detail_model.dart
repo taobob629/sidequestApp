aaf6230c4090c1994887728770a3c9eed778e2ea
zengchao928 <595018434@qq.com>
2023-04-23 10:04:30 +0800

    游戏主页增加Order

--- lib/ui/frame/profile/model/game_detail_model.dart
+++ lib/ui/frame/profile/model/game_detail_model.dart
@@ -1,47 +1,61 @@
+import '../other_profile/mdoel/player_info_mdoel.dart';
+
 class GameDetailModel {
   GameDetailModel({
     required this.voice,
+    required this.serviceItem,
     required this.backGround,
+    required this.rejectReason,
+    required this.gameName,
     required this.server,
     required this.intro,
     required this.style,
-    required this.position,
     required this.stars,
     required this.platform,
-    required this.gameName,
+    required this.status,
+    required this.position,
   });
 
   String voice;
+  List<ServiceItem> serviceItem;
   String backGround;
+  String rejectReason;
+  String gameName;
   String server;
   String intro;
   String style;
   String position;
   double stars;
   String platform;
-  String gameName;
+  int status;
 
   factory GameDetailModel.fromJson(Map<String, dynamic> json) => GameDetailModel(
     voice: json["voice"],
-    backGround: json["backGround"]??'',
+    serviceItem: List<ServiceItem>.from(json["serviceItem"].map((x) => ServiceItem.fromJson(x))),
+    backGround: json["backGround"],
+    rejectReason: json["rejectReason"],
+    position: json["position"] ?? '',
+    gameName: json["gameName"],
     server: json["Server"] ?? '',
     intro: json["intro"] ?? '',
     style: json["Style"] ?? '',
-    position: json["position"] ?? '',
-    stars: json["stars"]??5.0,
+    stars: json["stars"]?.toDouble(),
     platform: json["platform"],
-    gameName: json["gameName"],
+    status: json["status"],
   );
 
   Map<String, dynamic> toJson() => {
     "voice": voice,
+    "serviceItem": List<dynamic>.from(serviceItem.map((x) => x.toJson())),
     "backGround": backGround,
+    "rejectReason": rejectReason,
+    "gameName": gameName,
     "Server": server,
     "intro": intro,
     "Style": style,
-    "position": position,
     "stars": stars,
+    "position": position,
     "platform": platform,
-    "gameName": gameName,
+    "status": status,
   };
 }
