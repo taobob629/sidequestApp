bfc62cf09590405c4339b588930f1bfe45cb6cfc
zengchao928 <595018434@qq.com>
2023-04-25 14:58:43 +0800

    post照片不能超过5M

--- lib/ui/frame/social/post/contorller/release_post_controller.dart
+++ lib/ui/frame/social/post/contorller/release_post_controller.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:wy/api/common.dart';
@@ -30,7 +31,8 @@ class ReleasePostController extends GetxController {
   }
 
   pickUploadPhoto() async {
-    List<XFile>? files = await _picker.pickMultiImage(imageQuality: 30);
+    print('photoList = ${photoList.length}');
+    List<XFile>? files = await _picker.pickMultiImage();
     if (files.isEmpty == true) {
       return;
     }
@@ -43,6 +45,13 @@ class ReleasePostController extends GetxController {
     EasyLoading.show();
     for (int i = 0; i < files.length; i++) {
       File file = File(files[i].path);
+
+      // 最大5M
+      if (file.lengthSync() > 2 * 1024 * 1024) {
+        EasyLoading.dismiss();
+        showInfoDialog('The maximum size of the photo is 5MB'.tr);
+        return;
+      }
 
       Common.uploadFile(file, (p0, p1) {}).then((url) {
         if (url.isNotEmpty) {
@@ -63,7 +72,9 @@ class ReleasePostController extends GetxController {
       return;
     }
     EasyLoading.show();
-    PostApi.releasePost(content: textController.text, images: jsonEncode(photoList)).then((value) {
+    PostApi.releasePost(
+            content: textController.text, images: jsonEncode(photoList))
+        .then((value) {
       Get.back(result: "ReloadData");
     }).onError((error, stackTrace) {
       EasyLoading.dismiss();
