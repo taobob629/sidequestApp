0bc842674d5a2acd563239342db47204ca31f81d
machun <machun@essex.com.cn>
2023-04-27 22:13:18 +0800

    1.0.10版本号

--- lib/utils/storage_manager.dart
+++ lib/utils/storage_manager.dart
@@ -11,7 +11,8 @@ import 'package:wy/main.dart';
 import 'package:wy/model/credit_card_model.dart';
 import 'package:wy/model/user_model.dart';
 import 'package:wy/utils/utils.dart';
-const String default_server='dev184';//上线时候要改成prod
+//const String default_server='dev184';//上线时候要改成prod
+const String default_server='prod';//上线时候要改成prod
 class StorageManager {
   /// app全局配置
   static late SharedPreferences sharedPreferences;
