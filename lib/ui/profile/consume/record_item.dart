a1551e5f943cc7d185d56eae227dab7c8ff9cb40
taob <1277389320@qq.com>
2023-05-09 17:01:50 +0800

    上网记录

--- lib/ui/profile/consume/record_item.dart
+++ lib/ui/profile/consume/record_item.dart
@@ -379,7 +379,7 @@ class RecordItem extends StatelessWidget {
                     fontFamily: FONT_MEDIUM,
                   ),
                 ),
-                6.horizontalSpace,
+                3.horizontalSpace,
                 Container(
                   width: 60.w,
                   alignment: Alignment.centerRight,
@@ -396,7 +396,7 @@ class RecordItem extends StatelessWidget {
                 ),
               ],
             ),
-            10.verticalSpace,
+            7.verticalSpace,
             Align(
               alignment: Alignment.centerRight,
               child: Text(
