a8a4c83558ce27ed3ac774c4e6b59ba92a523cb1
taob <1277389320@qq.com>
2023-06-07 21:28:59 +0800

    Quest

--- lib/ui/frame/profile/my_profile/my_profile_page.dart
+++ lib/ui/frame/profile/my_profile/my_profile_page.dart
@@ -683,7 +683,7 @@ class MyProfilePage extends StatelessWidget {
                       Expanded(
                         child: _dashboardLabelItem(
                           "assets/images/profile/icon_task.webp",
-                          "Task".tr,
+                          "Quest".tr,
                           onTap: () => Get.to(() => TaskPage())
                               ?.then((value) => userController.updateInfo()),
                           badgeNum: userController.userProfile.taskNum,
