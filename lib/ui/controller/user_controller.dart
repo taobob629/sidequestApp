23e9a49f1681106909172dadf2aef14b85ddb789
davi <804033869@qq.com>
2023-07-27 10:20:34 +0800

    去掉录音检测

--- lib/ui/controller/user_controller.dart
+++ lib/ui/controller/user_controller.dart
@@ -210,7 +210,7 @@ class UserController extends GetxController {
       userProfile = await ProfileApi.getProfileInfo();
       //判断是否有语音
       if (hasDidVoiceCheck) return;
-      voiceCheck();
+    //  voiceCheck();
     }
   }
 
