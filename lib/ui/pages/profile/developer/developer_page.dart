3b6099553a0da266d0562c604247a9dfb97b228d
zengchao928 <595018434@qq.com>
2025-02-20 23:42:26 +0800

    优化

--- lib/ui/pages/profile/developer/developer_page.dart
+++ lib/ui/pages/profile/developer/developer_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:sq_hub_app/config/app_config.dart';
 
 import '../../../../common/base_scaffold.dart';
@@ -31,18 +32,41 @@ class DeveloperPage extends StatelessWidget {
                         color: Colors.white,
                         fontWeight: FontWeight.bold),
                   ),
+                  10.verticalSpace,
+                  GestureDetector(
+                    onTap: () {
+                      Clipboard.setData(
+                          ClipboardData(text: controller.pushToken.value));
+                      showError(
+                          "The Subscription ID has been copied to your clipboard"
+                              .tr);
+                    },
+                    child: Text("${controller.pushToken.value}",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                        )),
+                  ),
+                  10.verticalSpace,
+                  Text(
+                    "Onesignal ID".tr,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
                   SizedBox(
                     height: 10,
                   ),
                   GestureDetector(
                     onTap: () {
                       Clipboard.setData(
-                          ClipboardData(text: controller.pushToken.value));
-                      showToast(
-                          "The push token has been copied to your clipboard"
+                          ClipboardData(text: controller.onesignalId.value));
+                      showError(
+                          "The Onesignal ID has been copied to your clipboard"
                               .tr);
                     },
-                    child: Text("${controller.pushToken.value}",
+                    child: Text("${controller.onesignalId.value}",
                         style: TextStyle(
                           fontSize: 14,
                           color: Colors.white,
@@ -205,6 +229,8 @@ class DeveloperPage extends StatelessWidget {
 
 class DeveloperPageController extends GetxController {
   var pushToken = "".obs;
+  var onesignalId = "".obs;
+  var one = "".obs;
 
   var env = "prod".obs;
 
@@ -213,6 +239,15 @@ class DeveloperPageController extends GetxController {
     super.onReady();
     pushToken.value = StorageManager.getPushToken();
     env.value = StorageManager.getEnv();
+
+    getOnesignalId();
+  }
+
+  void getOnesignalId() async {
+    String? result = await OneSignal.User.getOnesignalId();
+    if (result != null) {
+      onesignalId.value = result;
+    }
   }
 
   void saveEnv() {
